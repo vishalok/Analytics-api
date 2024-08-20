887ff8c7876d70b84293629f48dcc95d7ecde5f9
vishalok <vrsafetech@gmail.com>
2024-08-20 16:01:55 +0530

    deploying code

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.json());
 const analyticsRoutes = require('./routes/analytics.route');
 app.use('/api/analytics', analyticsRoutes);
 
+app.get("/",(req,res)=>{
+    res.send("server is running!");
+})
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
